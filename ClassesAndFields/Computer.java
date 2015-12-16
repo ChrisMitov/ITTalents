9055d4d719c1be0fe9029bde5b665b02dfd086df
ChrisMitov <mitov.christian@gmail.com>
2015-12-16 17:01:00 +0200

    Classes and Fields

--- ClassesAndFields/Computer.java
+++ ClassesAndFields/Computer.java
@@ -20,34 +20,8 @@ public class Computer {
 	}
 
 	void print() {
-		System.out.println("Година на производство: " + year + "\nцена: " + price + "\nпреносим ли е: " + 
-				isNotebook + "\nразмер на харддиска: " + hardDiskMemory + "\nсвободна памет: " + freeMemory
-				+ "\nоперационна система: " + operationSystem);
-	}
-
-	public static void main(String[] args) {
-		Computer dell = new Computer();
-		dell.year = 1999;
-		dell.price = 100;
-		dell.isNotebook = false;
-		dell.hardDiskMemory = 160;
-		dell.freeMemory = 50;
-		dell.operationSystem = "Windows XP";
-
-		Computer hp = new Computer();
-		hp.year = 2009;
-		hp.price = 500.5;
-		hp.isNotebook = true;
-		hp.hardDiskMemory = 500;
-		hp.freeMemory = 264;
-		hp.operationSystem = "Ubuntu";
-
-		hp.useMemory(100);
-		dell.changeOperationSystem("Debian");
-		
-		System.out.println("DELL");
-		dell.print();
-		System.out.println("HP");
-		hp.print();
+		System.out.println("Year of production: " + this.year + ", Price: " + this.price + ", Is notebook: "
+				+ this.isNotebook + ", HardDisk memory: " + this.hardDiskMemory + ", Free momery: " + this.freeMemory
+				+ ", Operation system: " + this.operationSystem + ".");
 	}
 }
